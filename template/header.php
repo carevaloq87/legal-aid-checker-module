e1d3058c0cdc66d91df7a712a31fe773a2b90c8d
Christian Arevalo <carevaloq87@hotmail.com>
2017-02-13 07:11:08 +0000

    Update google tag manager

--- template/header.php
+++ template/header.php
@@ -4,6 +4,15 @@ $module_path = drupal_get_path('module', 'matter');
 <!DOCTYPE html>
 <html lang="<?= $vars['language']; ?>">
 	<head>
+		
+		<!-- Google Tag Manager -->
+		<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+		new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+		j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+		'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+		})(window,document,'script','dataLayer','GTM-TWDTW9B');</script>
+		<!-- End Google Tag Manager -->
+		
 		<title><?= $vars['title']; ?></title>
 		<meta charset="utf-8">
 		<link rel="shortcut icon" href="https://www.legalaid.vic.gov.au/profiles/vlapublic_profile/themes/custom/vlapublic_theme/favicon.ico" type="image/vnd.microsoft.icon" />
@@ -99,6 +108,10 @@ $module_path = drupal_get_path('module', 'matter');
 	</head>
 
 	<body>
+		<!-- Google Tag Manager (noscript) -->
+		<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TWDTW9B"
+		height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+		<!-- End Google Tag Manager (noscript) -->
 	    
 		<header>
 			<div class="logo"><img src="/<?= $module_path ?>/images/cla-logo.png" alt="Victoria Legal Aid Logo" title="Victoria Legal Aid Logo" tabindex="0"></div>
